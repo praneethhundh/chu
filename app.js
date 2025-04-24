58c7db9a62e9b768173c920e5f5347c294ba1edd
praneethhundh <130727307+praneethhundh@users.noreply.github.com>
2025-04-24 11:19:02 +0530

    app.js

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// Set view engine
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-// Middleware to parse form data
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// GET route for the form
+
 app.get('/register', (req, res) => {
   res.render('register');
 });
 
-// POST route to handle form submission
 app.post('/register', (req, res) => {
   const { name, age, nationality } = req.body;
   console.log("Registration Received:");
@@ -24,7 +23,6 @@ app.post('/register', (req, res) => {
   res.send("Registration successful!");
 });
 
-// Start server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
